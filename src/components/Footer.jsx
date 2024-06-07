3f8bc10c3cf5fe1b7db0029e723bc0ade27fabd0
arttturslv <arttturslv@gmail.com>
2024-06-07 14:03:45 -0300

    feat: transform footer from HTML/CSS to React component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <footer className="w-full flex flex-col justify-center items-center gap-6" >
+            <button onClick={() => console.log("ir para git")} type="button" className="ba hover:bg-jet hover:text-platinum  w-[500px]">
+                Veja mais no github!
+            </button>
+            <h6>
+                Este site não utiliza 🍪 :)
+            </h6>
+        </footer>
+    )
+}
