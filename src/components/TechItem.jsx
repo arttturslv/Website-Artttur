24edfa58006b7df6990926551fcf4f8c61e4d906
arttturslv <arttturslv@gmail.com>
2024-06-07 14:38:41 -0300

    style: improved mobile responsive

--- src/components/TechItem.jsx
+++ src/components/TechItem.jsx
@@ -3,9 +3,9 @@ export default function techItem({techName, techImage}) {
     return (
         <div className='ba w-min'>
 
-            <div className='px-2 w-max py-0.5 flex items-center gap-3'>
+            <div className='px-2 w-12 sm:w-max py-0.5 flex items-center gap-3'>
                 <img src={techImage} alt={`${techName} icon`} />
-                <h4 className=" text-[1.2rem] ">{techName}</h4>
+                <h4 className=" text-[1.2rem] hidden md:block ">{techName}</h4>
             </div>
         </div>
 
