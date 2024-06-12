1a8cf3e6855ea2c9c83224a74b0085d394308d77
arttturslv <arttturslv@gmail.com>
2024-06-12 10:21:41 -0300

    assets: added projects images

--- src/components/Home/Projetos.jsx
+++ src/components/Home/Projetos.jsx
@@ -1,11 +1,22 @@
-import DevelopmentTag from "../DevelopmentTag"
-import ContentTag from "../ContentTag"
-import githubIcon from '../../assets/icons/contact/Github.jsx'
 import ProjetoItem from './ProjetoItem'
 import Button from '../../components/Button.jsx'
 
 import puclove from '../../assets/projects/puclove.webp'
-import postit from '../../assets/projects/post-it.webp'
+import puclove1 from '../../assets/projects/puclove1.webp'
+import puclove2 from '../../assets/projects/puclove2.webp'
+
+import postit from '../../assets/projects/postaai.webp'
+import postit1 from '../../assets/projects/postaai1.webp'
+import postit2 from '../../assets/projects/postaai2.webp'
+
+import guru from '../../assets/projects/guru.webp'
+import guru1 from '../../assets/projects/guru1.webp'
+
+import jogodavelha from '../../assets/projects/jogodavelha.webp'
+
+import portifolio from '../../assets/projects/portifolio.webp'
+import portifolio1 from '../../assets/projects/portifolio1.webp'
+
 
 export default function Projetos({ title, text, githubURL }) {
 
@@ -16,26 +27,49 @@ export default function Projetos({ title, text, githubURL }) {
       <div id="" className='flex flex-wrap gap-12 py-3 px-2 justify-center'>
         <ProjetoItem
           title={"Post It 📋"}
-          text="Esse projeto é uma continuação do trabalho realizado para a disciplina de Desenvolvimento Web - FullStack, onde está sendo desenvolvido em React e Tailwind no frontend, Node.JS no backend e Mongo como Banco de Dados. A ideia principal deste projeto é disponilizar um site em que qualquer pessoa possa contribuir colocando anotações na homepage e futuramente, penso em adicionar mais funcionalidades, como cadastro e login para que os usuários possam salvar suas anotações, curtir, editar etc."
+          text="Este projeto foi desenvolvido com React e Tailwind no frontend, Node.js no backend e MongoDB como banco de dados. A ideia principal deste projeto foi disponibilizar um site em que qualquer pessoa pudesse contribuir colocando anotações na homepage."
           githubURL={'https://github.com/arttturslv/Posta-ai'}
-          image={postit}
+          websiteURL={'https://posta-ai.vercel.app/'}
+          initialImage={postit}
+          images={[postit1, postit2]}
           devProgress={'done'}
-          tags={['react', 'node.js', 'mongodb']}
+          tags={['react', 'node.js', 'mongodb', 'tailwind']}
+        />
+
+        <ProjetoItem
+          title={"Jogo da Velha 💘"}
+          text="Este projeto foi desenvolvido para praticar o uso do React Native, bem como a utilização do React Navigation (stack) e o suporte a múltiplos idiomas com o i18next, que alterna entre português e inglês. O jogo está disponível em APK no GitHub."
+          githubURL={'https://github.com/arttturslv/tic-tac-toe'}
+          initialImage={jogodavelha}
+          devProgress={'done'}
+          tags={['react-native', 'stack-navigation', 'i18next']}
         />
 
         <ProjetoItem
           title={"PUCLove 💘"}
-          text="Projeto gigante utilizando no frontend React e Tailwind, Java no backend e Mongo como Banco de Dados. Nesse trabalho fiquei responsavel pela parte do frontend, desenvolvendo grande parte do visual no Figma. E também fiquei responsável no codificamento das paginas Home, Matches, Politica de privacidade, Não encontrado e outras funcionalidades como componentes de interesses, requisições para o backend e a pesquisa de musica no spotify."
+          text="Projeto gigante que está sendo refatorado, onde utilizamos React e Tailwind no frontend, Java no backend e MongoDB como banco de dados. Nesse trabalho, fiquei responsável pela parte do frontend, desenvolvendo grande parte do visual no Figma. Também fiquei responsável pela codificação das páginas Home, Matches, Política de Privacidade, Não Encontrado e outras funcionalidades, como componentes de interesses, requisições para o backend e a pesquisa de música no Spotify."
           githubURL={'https://github.com/arttturslv/puclove'}
-          image={puclove}
+          initialImage={puclove}
+          images={[puclove1, puclove2]}
           devProgress={'done'}
-          tags={['react', 'java', 'mongodb']}
+          tags={['react', 'java', 'mongodb', 'tailwind']}
+        />
+
+        <ProjetoItem
+          title={"Guru sabedoria 💘"}
+          text='Este projeto foi desenvolvido utilizando HTML, CSS, JS e Bootstrap. O foco deste trabalho foi reforçar os conhecimentos sobre APIs, além de entender mais sobre animações CSS e o modo noturno. Também foi meu primeiro contato com o design de interfaces, onde, antes de tudo, pensei em como o website seria e como os elementos se complementariam. Foi possível aprender animações "infinite" para fazer o monge flutuar, o modo noturno que muda o fundo de azul para amarelo, a animação de digitação para exibir o conteúdo recebido pela API e também sobre efeitos sonoros e como os navegadores os bloqueiam até que o usuário interaja com a tela (aplicado ao clicar no monge).'
+          githubURL={'https://github.com/arttturslv/Guru-sabedoria'}
+          websiteURL={'https://arttturslv.github.io/Guru-sabedoria/'}
+          initialImage={guru}
+          images={[guru1]}
+          devProgress={'done'}
+          tags={['css', 'html', 'javascript']}
         />
         
       </div>
     </section>
     <div className=' flex justify-center w-[100%] h-16'>
-      <Button content={"Ver mais no Github"} action={console.log("aa")}></Button>
+      <Button content={"Ver mais no Github"} action={() =>window.open('https://github.com/arttturslv/')}></Button>
     </div>
     </>
   )
