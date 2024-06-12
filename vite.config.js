9a99815a64bd743015f3d1dbb47e73039a44f1b0
arttturslv <arttturslv@gmail.com>
2024-06-12 10:36:15 -0300

    build: changes for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Website-Artttur",
   css: {
     postcss: {
       plugins: [tailwindcss()],
