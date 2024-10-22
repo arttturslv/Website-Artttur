c789f8ab119b506cfbbaa2eda7751c9747525ea2
arttturslv <arttturslv@gmail.com>
2024-10-21 23:11:18 -0300

    feat: added posts for testing

--- src/components/Blog/Post.jsx
+++ src/components/Blog/Post.jsx
@@ -1,30 +1,54 @@
 import ContentTag from "../ContentTag";
 import { useState, useRef } from 'react';
-export default function Post({title, tags, text}) {
+export default function Post({id="45", title, tags, texts}) {
+    const [isOpen, setIsOpen] = useState(false)
+
+
+    function handleSeeMore() {
+        setIsOpen(!isOpen)
+        console.log(id)
+    }
+
+    function getTextsLength(texts) {
+        let length=0;
+
+        for(let i=0; i<texts.length; i++) {
+            length += texts[i].length;
+        }
+        return length;
+    }
 
     return (
-        <div className='sketch-border w-[80%] relative justify-center flex items-end'>
+        <div className='sketch-border max-sm:w-[96%] w-[80%]  relative justify-center flex items-end'>
             <div className='bg-platinum dark:bg-jet absolute w-[95%] self-center top-[-0.8rem] h-4'></div>
-            <div className='my-2 flex flex-col'>
-                <div className='pl-8 overflow-hidden space-y-1  text-left w-[95%]'>
+            <div className='my-2 flex flex-col w-full'>
+                <div className='max-sm:pl-2.5 pl-8 overflow-hidden space-y-1  text-left w-[95%]'>
                     <h3 className='text-[1.3rem] font-bold'>{title}</h3>
                     
                     <div className="flex gap-2 w-4 overflow-ellipsis">
                         {
-                            tags.map((content)=> (
-                                <ContentTag text={content}/>
+                            tags?.map((content, index)=> (
+                                <ContentTag key={index} text={content}/>
                             ))
                         }
                     </div>
 
-                    <div className="h-[6rem] overflow-hidden">
-                        <p>{text}</p>
+                    <div className={`${isOpen? "h-full" : "h-[6rem]"} overflow-hidden`}>
+                        {
+                            texts.map((item, index)=> (
+                                <p key={index}>{item}</p>
+                            ))
+                        }
                     </div>
                 </div>
-                <div className='group w-32 gap-2 transition-all flex self-end cursor-pointer'>
-                    <p className='text-right'>see more</p>
-                    <p className='text-right group-hover:rotate-90 delay-75 transition-all group-hover:tracking-widest	'> &#62;&#62;</p>
-                </div>
+                {
+                    getTextsLength(texts) > 325 && (
+                        <div onClick={handleSeeMore} className='group w-32 gap-2 transition-all flex self-end cursor-pointer'>
+                            <p className='text-right'>{isOpen? "see less": "see more"}</p>
+                            <p className={`${isOpen? "group-hover:-rotate-90": "group-hover:rotate-90"} text-right  delay-75 transition-all group-hover:tracking-widest`}> &#62;&#62;</p>
+                        </div>
+                    )
+                }
             </div>
             <div className=' bg-platinum dark:bg-jet  absolute w-[95%] self-center bottom-[-0.8rem] h-4'></div>
     </div>
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,35 +1,48 @@
+import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
 
-import Navbar from '../components/Navbar.jsx'
-import Footer from '../components/Footer.jsx'
-
-import Post from '../components/Blog/Post.jsx';
+import Post from "../components/Blog/Post.jsx";
 export default function Blog({ theme, setTheme }) {
-
   return (
-    <div id='at' className={`${theme ?? 'dark'}`}>
-      <div className='w-screen flex justify-center bg-platinum dark:bg-jet'>
-        <div className='dark:text-platinum h-screen  max-w-[1240px] w-[100%] space-y-8 text-jet'>
-          
+    <div id="at" className={`${theme ?? "dark"}`}>
+      <div className="max-sm:w-screen flex justify-center bg-platinum dark:bg-jet min-h-screen">
+        <div className="dark:text-platinum max-w-[1240px] w-full min-h-screen space-y-8 text-jet">
           <Navbar setTheme={setTheme} theme={theme} />
+          <div className="flex-col h-[80%]">
+            <div className="flex items-center flex-col">
+              <code className="flex text-[18px] gap-2">
+                <h1>&#x0007B;</h1>
+                <h1 className="opacity-50 w-7 text-clip transition-all overflow-clip whitespace-nowrap group-hover:w-[25rem]">
+                  basicamente um
+                </h1>
+                <h1
+                  className="w-[4.5rem] text-clip transition-all overflow-hidden group-hover:w-[7rem]"
+                  style={{ direction: "rtl", textAlign: "left" }}
+                >
+                  blog
+                </h1>
+                <h1>&#x0007D;</h1>
+              </code>
+              <Post
+                title="Template repository"
+                tags={["Github", "Tips"]}
+                texts={[
+                  "O GitHub é uma das plataformas mais populares para hospedagem de projetos e colaboração em código-fonte. Para desenvolvedores e equipes, ter um template de repositório padrão pode agilizar o processo de criação de novos projetos, garantindo que todos sigam as mesmas diretrizes e melhores práticas desde o início. Um template pode incluir estrutura de diretórios, arquivos de configuração, documentação e muito mais. Neste guia, vamos explorar como criar e utilizar um template de repositório padrão no GitHub. Então vamos lá, basta acessar sua conta, criar um novo repositorio, então adicione arquivos como pastas e documentos e modifique a README.md e a license. Depois de tudo isso feito, vá nas configurações do repositorio e marque como sim",
+                ]}
+              />
 
-          <div className='flex flex-col justify-center  items-center gap-4'>
-
-            <code className='flex text-[18px] gap-2 group'>
-              <h1>&#x0007B;</h1>
-              <h1 className='opacity-50 w-7 text-clip transition-all overflow-clip whitespace-nowrap group-hover:w-[25rem]'>basicamente um</h1>
-              <h1 class="w-[4.5rem] text-clip transition-all overflow-hidden group-hover:w-[7rem]" style={{ direction: 'rtl', textAlign: 'left' }}>blog</h1>
-              <h1>&#x0007D;</h1>
-            </code>
-
-          
+              <Post
+                title="Template repository"
+                tags={["Github", "Tips"]}
+                texts={[
+                  "ão em código-fonte. Para desenvolvedores e equipes, ter um template de repositório padrão pode agilizar o processo de criação de novos projetos, garantindo que todos sigam as mesmas diretrizes e melhores práticas desde o início. Um template pode incluir estrutura de diretórios, arquivos de configuração, documentação e muito mais. Neste guia, vamos explorar como criar e utilizar um template de repositório padrão no GitHub. Então vamos lá, basta acessar sua conta, criar um novo repositorio, então adicione arquivos como pastas e documentos e modifique a README.md e a license. Depois de tudo isso feito, vá nas configurações do repositorio e marque como sim",
+                ]}
+              />
+            </div>
           </div>
-
-
-
           <Footer></Footer>
-
         </div>
       </div>
     </div>
-  )
+  );
 }
